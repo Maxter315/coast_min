e41d413dd97318730ad0ddf193e9dd517bb1edfe
Maxter315 <maxter315@gmail.com>
2016-12-09 11:38:49 +0200

    init

---
+++ coast/coast.ino
@@ -0,0 +1,24 @@
+#define BUTTON 3
+#define SWITCH 4
+
+#define CLK_SR 0
+#define DATA_SR 1
+#define LATCH_SR 2
+
+#define LIGHTHOUSE_RED 5
+#define COASTHOUSE 6
+#define SHIP_YELLOW 7
+#define SHIP_RED 8
+
+
+void setup(){
+
+    pinMode(BUTTON,INPUT);
+    pinMode(SWITCH,INPUT);
+
+
+}
+
+void loop(){
+
+}
